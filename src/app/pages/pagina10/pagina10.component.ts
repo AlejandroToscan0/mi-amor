1894a34a5a612d9af6cd0a5ba1f92a4d007d1dd4
Jossue Toscano <105691569+AlejandroToscan0@users.noreply.github.com>
2025-02-16 17:08:21 -0500

    ultimos detalles

--- src/app/pages/pagina10/pagina10.component.ts
+++ src/app/pages/pagina10/pagina10.component.ts
@@ -1,6 +1,7 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterModule } from '@angular/router';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-pagina10',
@@ -9,5 +10,11 @@ import { RouterModule } from '@angular/router';
   styleUrl: './pagina10.component.css'
 })
 export class Pagina10Component {
-
+  title = 'mi-amor';
+  
+    constructor(private router: Router) {}
+  
+    irPagina11() {
+      this.router.navigate(['/pagina11']);
+    }
 }
