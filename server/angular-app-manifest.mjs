3a7cf27cb4887ddb0acd42b641de6b1653037964
Jossue Toscano <105691569+AlejandroToscan0@users.noreply.github.com>
2025-02-16 18:36:15 -0500

    Updates

---
+++ server/angular-app-manifest.mjs
@@ -0,0 +1,80 @@
+
+export default {
+  bootstrap: () => import('./main.server.mjs').then(m => m.default),
+  inlineCriticalCss: true,
+  baseHref: '/',
+  locale: undefined,
+  routes: [
+  {
+    "renderMode": 2,
+    "route": "/"
+  },
+  {
+    "renderMode": 2,
+    "route": "/pagina1"
+  },
+  {
+    "renderMode": 2,
+    "route": "/pagina2"
+  },
+  {
+    "renderMode": 2,
+    "route": "/pagina3"
+  },
+  {
+    "renderMode": 2,
+    "route": "/pagina4"
+  },
+  {
+    "renderMode": 2,
+    "route": "/pagina5"
+  },
+  {
+    "renderMode": 2,
+    "route": "/pagina6"
+  },
+  {
+    "renderMode": 2,
+    "route": "/pagina7"
+  },
+  {
+    "renderMode": 2,
+    "route": "/pagina8"
+  },
+  {
+    "renderMode": 2,
+    "route": "/pagina9"
+  },
+  {
+    "renderMode": 2,
+    "route": "/pagina10"
+  },
+  {
+    "renderMode": 2,
+    "route": "/pagina11"
+  },
+  {
+    "renderMode": 2,
+    "route": "/pagina12"
+  }
+],
+  entryPointToBrowserMapping: undefined,
+  assets: {
+    'index.csr.html': {size: 6041, hash: 'c017a06cca259c580471104801be175a58fd82877b368dda4848382fb80cb31b', text: () => import('./assets-chunks/index_csr_html.mjs').then(m => m.default)},
+    'index.server.html': {size: 1123, hash: '033080dda234f7dd055efdc7d5868d2c756f5706ac06499e24e45acea70caee0', text: () => import('./assets-chunks/index_server_html.mjs').then(m => m.default)},
+    'pagina1/index.html': {size: 10333, hash: '57adf07d260a1453d839fce186605450be639b8069e88984f63d568b8bd4bde9', text: () => import('./assets-chunks/pagina1_index_html.mjs').then(m => m.default)},
+    'pagina3/index.html': {size: 11318, hash: 'defb20444eee8ec6c85273581b2d530b994795889aca43955e02a2652c827af2', text: () => import('./assets-chunks/pagina3_index_html.mjs').then(m => m.default)},
+    'index.html': {size: 11028, hash: 'da58d9d04b69712a4a38ea22ac5de70f4988378ac160ce45b877824834ff16f7', text: () => import('./assets-chunks/index_html.mjs').then(m => m.default)},
+    'pagina4/index.html': {size: 11398, hash: '159c88562dc076d7244836c0343d288f487ad029f46e3fe4479ccb53bfdebc55', text: () => import('./assets-chunks/pagina4_index_html.mjs').then(m => m.default)},
+    'pagina2/index.html': {size: 13180, hash: '48292e9359f7f48f43f9aafdd2f99abf9cfea5035038da89795910a8b3f1141c', text: () => import('./assets-chunks/pagina2_index_html.mjs').then(m => m.default)},
+    'pagina7/index.html': {size: 11654, hash: 'b5f6d1c4abb1eb72cf6660911a137f05a253bf13c6978d292e94f8128250f60b', text: () => import('./assets-chunks/pagina7_index_html.mjs').then(m => m.default)},
+    'pagina8/index.html': {size: 11258, hash: 'a18282a1883bb71181236ebe2056db67c946a9e571cdda5d827cda2a1a77ddd8', text: () => import('./assets-chunks/pagina8_index_html.mjs').then(m => m.default)},
+    'pagina6/index.html': {size: 28512, hash: '58378a098e7f6f198724f94a56185699a3eb2b5f1935cadca24d861ea9e31e74', text: () => import('./assets-chunks/pagina6_index_html.mjs').then(m => m.default)},
+    'pagina9/index.html': {size: 11824, hash: 'c3e00ce7a965bdd96be4fb1bb8cb98fcbf821755f02f325fc394a634b5aa0bf1', text: () => import('./assets-chunks/pagina9_index_html.mjs').then(m => m.default)},
+    'pagina10/index.html': {size: 11342, hash: '82ae2ffee0877675fa8daa65a9c8e5cf0c070f0a3010344819f6adad8e78ec25', text: () => import('./assets-chunks/pagina10_index_html.mjs').then(m => m.default)},
+    'pagina11/index.html': {size: 11906, hash: 'f957b132cd0a681bd0d6e187f0eb8e80e63a7ee8d0a9c1bf2dbe4d88286f15ac', text: () => import('./assets-chunks/pagina11_index_html.mjs').then(m => m.default)},
+    'pagina12/index.html': {size: 11371, hash: 'e9b78b44cb724da3aed96dc1f1711373e747f186566fd13f59489ae9ddf28969', text: () => import('./assets-chunks/pagina12_index_html.mjs').then(m => m.default)},
+    'pagina5/index.html': {size: 12310, hash: '1cee00cfb019b2c0469de6b194586a83c5ea80313beafabdd634489de27d0002', text: () => import('./assets-chunks/pagina5_index_html.mjs').then(m => m.default)},
+    'styles-IHT3LI5Q.css': {size: 232646, hash: 'rMf8V5zr/D8', text: () => import('./assets-chunks/styles-IHT3LI5Q_css.mjs').then(m => m.default)}
+  },
+};
